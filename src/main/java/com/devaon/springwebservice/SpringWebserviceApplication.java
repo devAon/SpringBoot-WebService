5ac81fe9469cfa2ac637a6ad5d31b3529ee662c3
devAon <yewon9742@gmail.com>
2020-04-29 20:07:38 +0900

    first commit

---
+++ src/main/java/com/devaon/springwebservice/SpringWebserviceApplication.java
@@ -0,0 +1,15 @@
+package com.devaon.springwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebserviceApplication.class, args);
+    }
+
+}
